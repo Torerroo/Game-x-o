f31d558a62a069e3b58a3391a53e64fde8f879a6
Torerroo <Torerro1996@mail.ru>
2023-04-09 12:20:28 +0300

    start project

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+    {
+      files: ['scr/redux/slices/*Slice.js'],
+      rules: { 'no-param-reassign': ['error', { props: false }] },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: ['error', 'never'],
+    'linebreak-style': 0,
+    'no-console': 0,
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'import/no-cycle': 0,
+    'import/prefer-default-export': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-no-constructed-context-values': 0,
+    'no-param-reassign': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+  },
+}
